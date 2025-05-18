0ca958d4a31bd52cd0ae2a62aba272e95db68515
kanchisaw <siddheesaw@gmail.com>
2025-05-18 20:39:46 +0530

    build

---
+++ my-project/netlify-build.js
@@ -0,0 +1,6 @@
+// This is a placeholder file to satisfy Vercel's build process
+// It doesn't do anything but prevents build errors when deploying to Vercel
+
+console.log('Vercel deployment compatibility script');
+// Exit successfully
+process.exit(0);
