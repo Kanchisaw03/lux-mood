f4b79503da30d929c8c8b56c78fa775690630da7
kanchisaw <siddheesaw@gmail.com>
2025-05-18 20:44:00 +0530

    Remove netlify-build.js and postbuild script

--- my-project/netlify-build.js
+++
@@ -1,7 +0,0 @@
-// This is a placeholder file to satisfy Vercel's build process
-// It doesn't do anything but prevents build errors when deploying to Vercel
-
-console.log('Vercel deployment compatibility script');
-// Exit successfully
-// Use ES module syntax since package.json has "type": "module"
-export {}; // Empty export to make this a valid ES module
